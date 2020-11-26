da1db302cc9cf63faa305df7640e39aec4752124
MaciejJopek <297615@uwr.edu.pl>
2020-11-26 20:13:45 +0100

    add array with data from db

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,12 @@ from mongodb_connection.db_operations import *
 @app.route('/')
 def dashboard():
     temperature_array = get_value_for_sensor_for_last_days(7, 'temperatura')
-    print(temperature_array)
-    x = []
-    y = []
+    x_temperature = []
+    y_temperature = []
+    for value in temperature_array:
+        for key, value in value.items():
+            x_temperature.append(key)
+            y_temperature.append(value)
     temperature = read_data_from_db("temperatura")
     status_temperatury = int(temperature.split()[0])
     wartosc_temperatury = temperature.split()[1]
